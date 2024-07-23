b801b7c0db1e165f32582ef9f63de2dc4dddbc2d
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-07-23 11:50:59 +0530

    Add files via upload

--- Calculator.jsx
+++ Calculator.jsx
@@ -1,11 +1,24 @@
+import { useRef } from "react";
+import { findAllInRenderedTree } from "react-dom/test-utils";
+
 export default function Calculator() {
+
+
+    let textBox1=useRef(0);
+    let textBox2=useRef(0);
+
+const buttonHandler = ()=>{
+    let num1= parseInt(textBox1.current.value);
+    let num2= parseInt(textBox2.current.value);
+   alert("Sum is :"+(num1+num2));
+}
 
     return (
         <div>
             <h1>Calculator</h1>
-            <input type="text" placeholder="Enter Number 1" /><br/>
-            <input type="text" placeholder="Enter Number 2" />><br/>
-            <button>Add</button>
+            <input type="text" ref={textBox1} placeholder="Enter Number 1" /><br/>
+            <input type="text" ref={textBox2} placeholder="Enter Number 2" /><br/>
+            <button onClick={buttonHandler}>Add</button>
         </div>
     )
 }
