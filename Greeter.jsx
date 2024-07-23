5874c49e312f869e24680e733b2f0d245f75eb98
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-07-23 13:03:40 +0530

    Add files via upload

--- Greeter.jsx
+++ Greeter.jsx
@@ -4,14 +4,33 @@ export default class Greeter extends Component {
 
     constructor(){
         super();
-        this.name="Unknown";
+        // this.name="Unknown";
+        this.state={
+            name:"Unknown"
+        }
+        this.keyHandler = this.keyHandler.bind(this);  
+        this.changeHandler = this.changeHandler.bind(this); 
+    }
+
+    keyHandler(e){
+        console.log("Key Pressed",e.key);
+    }
+
+    changeHandler(e){
+        //this will trigger render method (UI will update)
+        this.setState({
+            name:e.target.value.toUpperCase()
+        })
     }
 
     render(){
         return (<div>
             <h1>Greeter</h1>
-            <input type="text" value={this.name} placeholder="Enter Name" />
-            <p>Hello, {this.name}</p>
+            <input type="text" value={this.state.name} 
+                // onKeyUp={this.keyHandler} 
+                onChange={this.changeHandler}
+                placeholder="Enter Name" />
+            <p>Hello, {this.state.name}</p>
         </div>)
     }
 }
